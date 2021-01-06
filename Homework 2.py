aed6f4d0c81a892132caef73ba0faeda7f6799c6
Adam Werbik <adamnwerbik@gmail.com>
2021-01-07 01:01:15 +1100

    Exercises file
    File created and first exercise done

--- Homework 2.py
+++ Homework 2.py
@@ -1,1 +1,9 @@
-a
+# Exercise 2.2
+
+def is_divisible(m, n):
+    return (m/n == 0 )
+
+#test cases
+print(is_divisible(3,1)) #should return True
+print(is_divisible(35,2)) #should return False
+print(is_divisible(3,50)) #should return False
