875a5a93c427a61e4f683651b8f5eadeccf34bea
Adam Werbik <adamnwerbik@gmail.com>
2021-01-07 23:31:32 +1100

    Part C almsot done

--- Problem Set 1 Rewrite.py
+++ Problem Set 1 Rewrite.py
@@ -1,3 +1,4 @@
+"""
 #PART A
 annual_salary = float(input("Enter your annual salary: "))
 prop_saved = float(input("Enter the percent of your salary to save, as a decimal:"))
@@ -38,19 +39,28 @@ print(months)
 
 #All three test cases correct.
 
+
 #Part C - Finding the right amount of money to save away
-starting_salary = float(input("Enter the starting salary: "))
-num_bisections = 0
+
 #finding the best savings rate through teh bisection method. 
 #Assumptions: dreamhouse price is $1m, 25% downpayment, semi_annual 
 #raise is 0.07, annual return of investments is 0.04, want dwpmnt within
 #36 months
 
+import math
 possibilities = [x for x in range(0,10001)]
+starting_salary = float(input("Enter the starting salary: "))
 num_bisections = 0
-annual_salary = float(input("Enter your annual salary: "))
+#other vars as given in the question
+annual_return = 0.04
+semi_annual_raise = 0.07
+total_cost = 1000000
+downpayment_prop = 0.25
+
+j=0
 while True:
-    mid_point = (possibilities[0] + possibilities[-1])/2
+    j += 1
+    mid_point = math.ceil((possibilities[0] + possibilities[-1])/2)
     print(f"midpoint is {mid_point}")
     #work out how much they'd save in the given timeframe, then compare, 
     #and adjust range (discard half)
@@ -58,15 +68,73 @@ while True:
     months = 0
     for i in range(0,36+1):
         months += 1
-        investment_returns = savings * (0.04/12)
-        savings += investment_returns + annual_salary 
+        investment_returns = savings * (annual_return/12)
+        savings += investment_returns + (mid_point/100)*(starting_salary/12)
+        if i%6 ==0:
+            starting_salary = (1+semi_annual_raise)*starting_salary
+    if abs(savings-total_cost) < 100:
+        print(f"Best savings rate: {mid_point/100}")
+        print(f"Steps in bisection search: {num_bisections}")
+        break
+    else:
+        if savings > downpayment_prop * total_cost: #remove midpt and above
+            possibilities = possibilities[0:possibilities.index(mid_point)]
+        else:
+            possibilities = possibilities[possibilities.index(mid_point):]
+        num_bisections+=1
+    if j==3:
+        break
         
 
 
+possibilities = [x for x in range(0,10001)]
 
-    if saved_up
+import math
+for i in range(5):
+    print(i)
+    mdpt = math.ceil((possibilities[0] + possibilities[-1])/2)
+    print(f"{mdpt} is the midpoint.")
+    possibilities = possibilities[possibilities.index(mdpt):]
+    print(len(possibilities))
     
+"""
 
-mid_point = (possibilities[0] + possibilities[-1])/2
-print(f"midpoint is {mid_point}")
-saving_proportion = 
+
+import math
+possibilities = [x for x in range(0,10000+1)]
+total_price = 1000000
+deposit_prop = 0.25
+annual_return = 0.04
+semi_annual_raise = 0.07
+deposit_needed = deposit_prop * total_price
+bisections = 0
+starting_salary = float(input("Enter the starting salary: "))
+
+while True:
+    savings = 0
+    print("savings = zero")
+    midpoint = math.ceil((possibilities[0]+possibilities[-1])/2)
+    print(f"mdpt: {midpoint}")
+    for month in range(0, 36+1):
+        investment_returns = savings * (annual_return/12)
+        monthly_wage = starting_salary/12
+        savings += (midpoint/10000) * (monthly_wage) + investment_returns
+        if month%6 == 0 and month>1:
+            starting_salary *= (1+semi_annual_raise)
+        else:
+            ()
+    if abs(savings - deposit_needed) <= 100:
+        break
+    elif savings > deposit_needed: #saved too much, so "optimal" saving rate below midpt
+        possibilities = possibilities[0:possibilities.index(midpoint)]
+        print("Saved up too much!")
+    else:
+        print("Saved up too little!")
+        possibilities = possibilities[possibilities.index(midpoint):len(possibilities)]
+    bisections += 1
+    print(f"Savings: {savings}")
+    savings = 0
+
+print(f"Best savings rate: {midpoint/1000}")
+print(f"Steps in bisection search: {bisections}")
+    
